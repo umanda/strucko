024067eb7cfe6f5bc2d18e69c20dfe9a550ea33a
Marko Ivančić <cicnavi@gmail.com>
2015-11-04 22:16:42 +0100

    Implement footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,27 @@
-<ul class="nav nav-pills">
-  ...
-  <li role="presentation" class="disabled"><a href="#">Disabled link</a></li>
-  ...
-</ul>
+<footer class="footer">
+    <div class="well">
+        <a href="#" class="pull-right">Back to top</a>
+        
+        <ul class="nav nav-pills">
+            <li role="presentation"><a href="#" class="text-muted">Privacy Policy</a></li>
+            <li role="presentation"><a href="#" class="text-muted">Disclaimer</a></li>
+            <li role="presentation"><a href="#" class="text-muted">About Strucko</a></li>
+        </ul>
+        
+        <p class="text-muted text-center">
+            <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+                <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" />
+            </a>
+            <br />
+            <span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">
+                Terms and Definitions
+            </span> by 
+            <a xmlns:cc="http://creativecommons.org/ns#" href="http://strucko.com" property="cc:attributionName" rel="cc:attributionURL">
+                Strucko The Expert Dictionary
+            </a> are licensed under a 
+            <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+                Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
+        </p>
+    </div>
+    
+</footer>

f84479e5eca19b3a5d166c17f7ce1ccf7548f055
Marko Ivančić <cicnavi@gmail.com>
2016-04-12 20:46:49 +0200

    Fix index search paginator to show more than 2 terms

--- app/Http/Controllers/TermsController.php
+++ app/Http/Controllers/TermsController.php
@@ -119,7 +119,7 @@ class TermsController extends Controller
                         ->where($searchFilters)
                         ->with('partOfSpeech', 'language', 'status')
                         ->orderBy('term')
-                        ->paginate(2);
+                        ->paginate();
                 
                 // If the translate_to is set, get approved translations.
                 if ($this->filters->isSetTranslateTo()) {
